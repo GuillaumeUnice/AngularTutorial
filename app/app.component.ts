56eefd40beb0738e89f90eeeb883b41a7184f41d
GuillaumeUnice <guillaume.unice@gmail.com>
2016-06-18 16:06:29 +0200

    [Component Interaction] ngOnChanges in Child in order to catch input

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 
 import {ChildComponent} from './components/child/child.component';
- 
- @Component({
-   selector: 'my-app',
-   templateUrl: './app/app.component.html',
-   styleUrls: ['./app/app.component.css'],
-   providers: [],
-   directives: [ChildComponent]
- })
- export class AppComponent {
- 	parentVar : string = "myParentVar";
-   	constructor(){}
- 
- }
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css'],
+  providers: [],
+  directives: [ChildComponent]
+})
+export class AppComponent {
+public parentVar : string = "myParentVar";
+  constructor(){}
+
+  myFunc() {
+    this.parentVar = "myNewParentVar";
+  }
+}
