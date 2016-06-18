b599d212bf770f1876e1a7587386da370397bd68
GuillaumeUnice <guillaume.unice@gmail.com>
2016-06-18 16:14:45 +0200

    [Service] Dependency Injection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,25 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {ChildComponent} from './components/child/child.component';
+import {MyService} from './services/my-service/my-service.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app/app.component.html',
   styleUrls: ['./app/app.component.css'],
-  providers: [],
+  providers: [MyService],
   directives: [ChildComponent]
 })
 export class AppComponent implements OnInit {
   
-  constructor(){}
-
-  // Input communication
-  public parentVar : string = "myParentVar";
-  
-  //Changement I/O Child Component
-  myFunc() {
-    this.parentVar = "myNewParentVar";
-  }
-  
-  // Ouput communication
-  receiveOutputChildFunc(outputChildValue: string) {
-    this.parentVar = outputChildValue;
-  }
-  
-  //parent inject child
-  public parentChildVarInject: string;
-  @ViewChild(ChildComponent)
-  private _childComponent: ChildComponent;
-
+  constructor(private _myService : MyService){}
+ 
   ngOnInit() {
-    //this.parentChildVarInject = this._childComponent.childVarInject;
-    setTimeout( () => { this.parentChildVarInject = this._childComponent.childVarInject; }, 0);
+    console.log(this._myService.varService);
   }
+
+  parentFunction() {
+    this._myService.changeVarService();
+ 	}
 
 }
