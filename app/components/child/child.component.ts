6d08fa67bbe9f832be5eb2085d9ddd76abbe2082
GuillaumeUnice <guillaume.unice@gmail.com>
2016-06-18 16:06:29 +0200

    [Component Interaction] ngOnChanges in Child in order to catch input

--- app/components/child/child.component.ts
+++ app/components/child/child.component.ts
@@ -1,15 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChange} from '@angular/core';
  
- @Component({
-   selector: 'child',
-   templateUrl: 'app/components/child/child.component.html',
-   styleUrls: ['app/components/child/child.component.css'],
-   providers: []
- 
- })
- export class ChildComponent {
-   constructor(){}
- 
-   @Input('myAliasInputChild') myInputChild : string;
- 
- }
+@Component({
+  selector: 'child',
+  templateUrl: 'app/components/child/child.component.html',
+  styleUrls: ['app/components/child/child.component.css'],
+  providers: []
+
+})
+export class ChildComponent implements OnChanges {
+  constructor(){}
+
+  public changeLog: string[] = [];
+  @Input('myAliasInputChild') myInputChild : string;
+  
+  ngOnChanges(changes : {[propKey : string] : SimpleChange}) {
+    for(let propName in changes) {
+      let changedProp = changes[propName];
+      let from = JSON.stringify(changedProp.previousValue);
+      let to = JSON.stringify(changedProp.currentValue);
+      this.changeLog.push(`${propName} from ${from} to ${to}`);
+    }
+  }
+}
