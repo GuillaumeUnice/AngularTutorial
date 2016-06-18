e6b17240d218dda5804e645bf824aeafd111c1da
GuillaumeUnice <guillaume.unice@gmail.com>
2016-06-18 16:10:57 +0200

    [Component Interaction] parent inject child communication Child To Parent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 import {ChildComponent} from './components/child/child.component';
 
@@ -9,15 +9,31 @@ import {ChildComponent} from './components/child/child.component';
   providers: [],
   directives: [ChildComponent]
 })
-export class AppComponent {
-public parentVar : string = "myParentVar";
+export class AppComponent implements OnInit {
+  
   constructor(){}
 
+  // Input communication
+  public parentVar : string = "myParentVar";
+  
+  //Changement I/O Child Component
   myFunc() {
     this.parentVar = "myNewParentVar";
   }
   
+  // Ouput communication
   receiveOutputChildFunc(outputChildValue: string) {
     this.parentVar = outputChildValue;
   }
+  
+  //parent inject child
+  public parentChildVarInject: string;
+  @ViewChild(ChildComponent)
+  private _childComponent: ChildComponent;
+
+  ngOnInit() {
+    //this.parentChildVarInject = this._childComponent.childVarInject;
+    setTimeout( () => { this.parentChildVarInject = this._childComponent.childVarInject; }, 0);
+  }
+
 }
